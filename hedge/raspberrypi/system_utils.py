87917c77bdaacf3ca0193b40ddca335e28b12193
y0zhyk <lushney@gmail.com>
2013-09-21 08:40:35 +0300

    Did site compatible with Windows platform

--- hedge/raspberrypi/system_utils.py
+++ hedge/raspberrypi/system_utils.py
@@ -1,27 +1,44 @@
-import psutil
 import os
 
 def cpu_usage():
     """Retrieves information about cpu usage"""
-    return psutil.cpu_percent()
+    try:
+        import psutil
+        return psutil.cpu_percent()
+    except ImportError:
+        return 0
 
 
 def memory_usage():
     """Retrieves information about memory usage"""
-    return psutil.virtual_memory().percent
+    try:
+        import psutil
+        return psutil.virtual_memory().percent
+    except ImportError:
+        return 0
 
 
 def swap_usage():
     """Retrieves information about swap memory usage"""
-    return psutil.swap_memory().percent
+    try:
+        import psutil
+        return psutil.swap_memory().percent
+    except ImportError:
+        return 0
 
 
 def disk_usage():
     """Retrieves information about swap disk usage"""
-    return psutil.disk_usage('/').percent
-
+    try:
+        import psutil
+        return psutil.disk_usage('/').percent
+    except ImportError:
+        return 0
 
 def cpu_temperature():
     """Retrieves information about CPU temperature"""
     res = os.popen('vcgencmd measure_temp').readline()
-    return res.replace("temp=", "").replace("'C\n", "")
+    try:
+        return float(res.replace("temp=", "").replace("'C\n", ""))
+    except ValueError:
+        return 0
