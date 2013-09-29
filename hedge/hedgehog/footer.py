d03c2a8bea40f9d6037585f921dfd6dd7f67dd41
y0zhyk <lushney@gmail.com>
2013-09-29 11:26:23 +0300

    Updated footer and navigation menu view

---
+++ hedge/hedgehog/footer.py
@@ -0,0 +1,29 @@
+
+
+class LogoItem():
+    def __init__(self, image, url):
+        self.__image = image
+        self.__url = url
+
+    @property
+    def image(self):
+        return "images/{}".format(self.__image)
+
+    @property
+    def url(self):
+        return "http://{}".format(self.__url)
+
+
+class Logos(list):
+    """Represents site footer"""
+    def __init__(self, logos):
+        super(Logos, self).__init__()
+        self.extend(logos)
+
+logos = Logos(
+    [
+        LogoItem(image="debian_logo.png", url="www.raspbian.org"),
+        LogoItem(image="raspberrypi_logo.png", url="www.raspberrypi.org"),
+        LogoItem(image="python_logo.png", url="www.python.org")
+    ]
+)
