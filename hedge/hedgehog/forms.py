179d2ac01a4c830d442948d0fe9d0fd3df4167be
y0zhyk <lushney@gmail.com>
2013-10-12 10:25:50 +0300

    Did cosmetic change on login page

--- hedge/hedgehog/forms.py
+++ hedge/hedgehog/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms.util import ErrorList
+
+
+class _ErrorList(ErrorList):
+
+    def __unicode__(self):
+        if not self:
+            return u''
+        return u'{}'.format(self[0])
 
 
 class LoginForm(forms.Form):
     """Form for providing user credentials"""
-    username = forms.CharField(label="User name", required=False)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput, required=False)
+    username = forms.CharField(label="User name")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    def __init__(self, **kwds):
+        super(LoginForm, self).__init__(error_class=_ErrorList, empty_permitted=True, **kwds)
 
     def clean(self):
         raise ValidationError("Failed to login")
--- hedge/static/coffeescript/stats.coffee
+++ hedge/static/coffeescript/stats.coffee
@@ -2,7 +2,7 @@ showStats = () ->
   $.getJSON 'api/stats', (data) ->
     for stat in data.stats
       $("aside").append "#{stat.description}:<swap class=value id=#{stat.id}_value>#{stat.value}</swap>"
-      $("aside").append """<div class=meter><span id=#{stat.id}_percent/></div>"""
+      $("aside").append "<div class=meter><span id=#{stat.id}_percent/></div>"
       $("##{stat.id}_percent").width "#{stat.percent}%"
     $("aside").append "<p>Updated: <span id=time>#{data.time}</span>"
     setTimeout updateStats, 10000
