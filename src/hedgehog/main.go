c62c136e74fe32c88d07a0693a3e1ca4b76f449b
y0zhyk <lushney@gmail.com>
2014-04-25 23:07:41 +0300

    Added new version of the site

---
+++ src/hedgehog/main.go
@@ -0,0 +1,19 @@
+// hedgehog project main.go
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	// main page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./templates/hedgehog.html")
+	})
+
+	// static resources
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	http.ListenAndServe(":8082", nil)
+}
